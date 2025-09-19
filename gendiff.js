50ddd13df8d82dbb2702ed6e7392d40c5d4f4704
PxHA <Daniilkulbakov@gmail.com>
2025-09-19 18:59:20 +0300

    Выполнил 2 шаг

--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { readFileSync } from 'node:fs';
 
 const program = new Command();
 
@@ -7,5 +8,14 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>', 'первый путь до файла')
+    .argument('<filepath2>', 'второй путь до файла')
+    .action((filepath1, filepath2) => {
+        console.log(filepath1)
+        console.log(filepath2)
+        readFileSync(filepath1)
+        readFileSync(filepath2)
+    })
 
 program.parse();
