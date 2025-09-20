b20d0faf8eada661ec09db47e99ebbb02af865b2
PxHA <Daniilkulbakov@gmail.com>
2025-09-20 17:25:06 +0300

    Изменил файл gendiff

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import { readFileSync } from 'node:fs';
+import path from 'node:path';
 
 const program = new Command();
 
@@ -12,10 +13,14 @@ program
     .argument('<filepath1>', 'первый путь до файла')
     .argument('<filepath2>', 'второй путь до файла')
     .action((filepath1, filepath2) => {
-        console.log(filepath1)
-        console.log(filepath2)
+        filepath1 = path.resolve(process.cwd(), filepath1)
+        filepath2 = path.resolve(process.cwd(), filepath2)
         readFileSync(filepath1)
         readFileSync(filepath2)
     })
 
 program.parse();
+
+// относительный путь
+// зафиксировать пути сначала path.resolve и process.cwd и скормить корректный путь readfileSync 
+// надо понять как парсить разные расширения и парсить их в объект
