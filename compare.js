36055abc91a473a035f3339361d1a37799088c72
PxHA <Daniilkulbakov@gmail.com>
2025-10-30 10:09:34 +0300

    Остановил прохождения проекта на 7 шаге

--- compare.js
+++ compare.js
@@ -1,56 +1,24 @@
 import _ from 'lodash';
 
-const makeIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
-const makeBracketIndent = (depth, spacesCount = 4) => ' '.repeat((depth - 1) * spacesCount);
-
-export const compare = (data1, data2, depth = 1) => {
-  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-
-  const lines = keys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    const indent = makeIndent(depth);
-
-    // если ключа нет в первом, но есть во втором
-    if (!Object.hasOwnProperty.call(data1, key)) {
-      return `${indent}+ ${key}: ${formatValue(value2, depth + 1)}`;
+export const compare = (data1, data2) => {
+  let str = '';
+  const keys = _.sortedUniq(Object.keys(data1).concat(Object.keys(data2)).sort());
+  
+  for (const key of keys) {
+    if ('object' === typeof data1[key]) {
+      return compare(data1[key], data2[key])
     }
-
-    // если ключа нет во втором, но есть в первом
-    if (!Object.hasOwnProperty.call(data2, key)) {
-      return `${indent}- ${key}: ${formatValue(value1, depth + 1)}`;
+    if (!Object.hasOwnProperty.call(data1, key) && Object.hasOwnProperty.call(data2, key)) {
+      str += `  + ${key}: ${data2[key]}\n`;
+    } else if (Object.hasOwnProperty.call(data1, key) && !Object.hasOwnProperty.call(data2, key)) {
+      str += `  - ${key}: ${data1[key]}\n`;
+    } else if (Object.hasOwnProperty.call(data1, key) && Object.hasOwnProperty.call(data2, key)
+    && data1[key] === data2[key]) {
+      str += `    ${key}: ${data1[key]}\n`;
+    } else {
+      str += `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`;
     }
-
-    // если оба значения — объекты → рекурсивно
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return `${indent}  ${key}: ${compare(value1, value2, depth + 1)}`;
-    }
-
-    // если значения равны
-    if (_.isEqual(value1, value2)) {
-      return `${indent}  ${key}: ${formatValue(value1, depth + 1)}`;
-    }
-
-    // если значения разные
-    return [
-      `${indent}- ${key}: ${formatValue(value1, depth + 1)}`,
-      `${indent}+ ${key}: ${formatValue(value2, depth + 1)}`
-    ].join('\n');
-  });
-
-  return `{\n${lines.join('\n')}\n${makeBracketIndent(depth)}}`;
-};
-
-// вспомогательная функция для форматирования значений
-const formatValue = (value, depth) => {
-  if (!_.isPlainObject(value)) {
-    return String(value);
   }
-
-  const entries = Object.entries(value).map(
-    ([key, val]) => `${makeIndent(depth + 1)}  ${key}: ${formatValue(val, depth + 1)}`
-  );
-
-  return `{\n${entries.join('\n')}\n${makeBracketIndent(depth + 1)}}`;
+  
+  return `{\n${str}}`;
 };
-
